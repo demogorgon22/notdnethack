4cff93b2f00ace18f12628a5edb81d6abd94f975
demogorgon22 <sammagnus22@yahoo.com>
2021-10-01 10:39:45 -0400

    Fix falchion giving inc dmg still and add spire changes to changelog including new tat prices.

--- src/sounds.c
+++ src/sounds.c
@@ -858,7 +858,6 @@ boolean chatting;
 				if (smith_offer_price(charge, mtmp) == FALSE) break;
 				u.utats |= selection;
 				pline("%s tattoos a %s onto you.",Monnam(mtmp),tat_to_name(selection));
-				u.udaminc++;
 				You_feel("more ready for battle.");
 				break;
 			case TAT_KESTREL:
