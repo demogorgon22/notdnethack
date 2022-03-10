ed5e8e4237a9c8cbc13caa348f9d98b1c77e45a6
demogorgon22 <sammagnus22@yahoo.com>
2022-03-10 16:03:10 -0500

    Probably fix issue with being able to mine petrified trees by killing them which would cause an impossible when monsters did it.

--- src/hack.c
+++ src/hack.c
@@ -551,7 +551,8 @@ register xchar x,y;
 {
     return (boolean)!((levl[x][y].wall_info & W_NONDIGGABLE)
 		&&((IS_STWALL(levl[x][y].typ) ||
-			IS_TREE(levl[x][y].typ)
+			IS_TREE(levl[x][y].typ) || 
+			IS_TREES(levl[x][y].typ)
 		)));
 }
 
