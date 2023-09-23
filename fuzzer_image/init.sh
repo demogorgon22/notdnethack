2aed39eceff66ba118b585375c1fe6c700422e8a
demogorgon22 <sammagnus22@yahoo.com>
2023-09-23 21:05:28 +0000

    Rewrite fuzzer image to allow for versioned fuzzing.

--- fuzzer_image/init.sh
+++ fuzzer_image/init.sh
@@ -3,23 +3,25 @@ sysctl -w kernel.core_pattern=/tmp/cores/core-%e.%p.%h.%t
 cd /app/dnh/notdnethackdir
 /app/dnh/notdnethackdir/notdnethack -D -u `hostname` > /dev/null
 sleep 10
-cp /tmp/cores/* /cores
+CORES_DIR="/cores/notdnethack-$(git rev-parse HEAD)"
+mkdir $CORES_DIR
+cp /tmp/cores/* $CORES_DIR
 if [ -e /cores/dnh/notdnethackdir/notdnethack ]
-then
-	echo "nDNH install exists, checking hash"
-	ourHash=$(md5sum /app/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
-	theirHash=$(md5sum /cores/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
-	echo "$ourHash, $theirHash"
-	if [[ "$ourHash" == "$theirHash" ]]
 	then
-		echo "nDNH install in date, exiting"
+    	echo "nDNH install exists, checking hash"
+    	ourHash=$(md5sum /app/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
+    	theirHash=$(md5sum $CORES_DIR/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
+    	echo "$ourHash, $theirHash"
+    	if [[ "$ourHash" == "$theirHash" ]]
+        	then
+                echo "nDNH install in date, exiting"
+        	else
+                echo "nDNH install out of date, updating"
+                rm -rf $CORES_DIR/dnh
+                cp -r /app/dnh $CORES_DIR/dnh
+    	fi
 	else
-		echo "nDNH install out of date, updating"
-		rm -rf /cores/dnh
-		cp -r /app/dnh /cores/dnh
-	fi
-else
-	echo "nDNH install nonexistent, updating"
-	rm -rf /cores/dnh
-	cp -r /app/dnh /cores/dnh
+    	echo "nDNH install nonexistent, updating"
+    	rm -rf $CORES_DIR/dnh
+    	cp -r /app/dnh $CORES_DIR/dnh
 fi
