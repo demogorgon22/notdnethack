f6c5c496c7eca91b126c5341905189ac4cf17a02
demogorgon22 <sammagnus22@yahoo.com>
2024-01-18 16:44:16 -0500

    Try to simplify fuzzer source/binary copy over logic now that binaries should always match.

--- fuzzer_image/init.sh
+++ fuzzer_image/init.sh
@@ -6,22 +6,7 @@ sleep 10
 CORES_DIR="/cores/notdnethack-$(git rev-parse HEAD)"
 mkdir $CORES_DIR
 cp /tmp/cores/* $CORES_DIR
-if [ -e /cores/dnh/notdnethackdir/notdnethack ]
+if [ ! -d /cores/dnh ]
 	then
-    	echo "nDNH install exists, checking hash"
-    	ourHash=$(md5sum /app/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
-    	theirHash=$(md5sum $CORES_DIR/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
-    	echo "$ourHash, $theirHash"
-    	if [[ "$ourHash" == "$theirHash" ]]
-        	then
-                echo "nDNH install in date, exiting"
-        	else
-                echo "nDNH install out of date, updating"
-                rm -rf $CORES_DIR/dnh
-                cp -r /app/dnh $CORES_DIR/dnh
-    	fi
-	else
-    	echo "nDNH install nonexistent, updating"
-    	rm -rf $CORES_DIR/dnh
     	cp -r /app/dnh $CORES_DIR/dnh
 fi
