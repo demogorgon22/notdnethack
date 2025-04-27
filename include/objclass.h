3c275a7b0947fd497129c58a3b93de92ee1ffb4f
ChrisANG <chris@none.none>
2025-04-26 20:24:01 -0400

    Long swords shouldn't graze in pommel-mode

--- include/objclass.h
+++ include/objclass.h
@@ -241,6 +241,7 @@ struct objclass {
 										( ((obj)->expert_traits&trait  \
 											&& (trait == ETRAIT_FOCUS_FIRE || (obj)->otyp != TOOTH || !((obj)->o_e_trait&ETRAIT_FOCUS_FIRE)) \
 											&& !((mon) == &youmonst && objects[(obj)->otyp].oc_skill == P_LANCE && !(u.usteed || centauroid(youracedata) || animaloid(youracedata))) \
+											&& !(trait == ETRAIT_GRAZE && (mon) == &youmonst && obj->otyp == LONG_SWORD && activeFightingForm(FFORM_POMMEL)) \
 										) \
 										|| (trait == ETRAIT_QUICK && (mon) == &youmonst && obj->otyp == LONG_SWORD && activeFightingForm(FFORM_HALF_SWORD))\
 										|| (trait == ETRAIT_PENETRATE_ARMOR && (mon) == &youmonst && obj->otyp == LONG_SWORD && activeFightingForm(FFORM_POMMEL))\
