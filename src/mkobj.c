8303c5a458450dcca52d28b7980e7a54a12c649d
demogorgon22 <sammagnus22@yahoo.com>
2025-05-22 21:29:14 -0400

    We've been bad. No ring of thror multiplying gold. Why?  I'm not fixing it.

--- src/mkobj.c
+++ src/mkobj.c
@@ -3050,7 +3050,8 @@ int x, y;
 {
 	struct obj *gold;
 	gold = mkgold_core(amount, x, y, TRUE);
-	if(uring_art(ART_RING_OF_THROR)){
+	// Not fixing this rn so sleep tight
+	/*if(uring_art(ART_RING_OF_THROR)){
 		if(gold->quan&0x1L){//Odd piles stay odd
 			if(rn2(2))
 				gold->quan = 2*gold->quan + 1;
@@ -3060,7 +3061,7 @@ int x, y;
 		else
 			gold->quan = 2*gold->quan;
 		gold->owt = weight(gold);
-	}
+	}*/
 	return gold;
 }
 
