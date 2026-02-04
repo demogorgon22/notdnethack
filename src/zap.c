03c55196847de314e0fe36d1303b588e269aedb8
ChrisANG <chris@none.none>
2026-02-03 20:14:06 -0500

    Isolation suits also protect the skin from gray mold

--- src/zap.c
+++ src/zap.c
@@ -5146,7 +5146,7 @@ struct zapdata * zapdata;
 
 		return xdamagey(magr, mdef, &attk, dmg);
 	case AD_GMLD:
-	if(!acidic(youdef ? youracedata : mdef->data)){
+	if(!acidic(youdef ? youracedata : mdef->data) && !isolation_suit){
 		domsg();
 		boolean breathless = youdef ? Breathless : breathless_mon(mdef);
 		if(youdef){
